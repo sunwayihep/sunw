6ce13029b716f102b793a6f8a44ad8f802e36149
YellowBicycleee <wangjianchengcs@163.com>
2024-11-25 21:25:42 +0800

    modify: 修改了变量意义

--- tool/utils/insert_point.py
+++ tool/utils/insert_point.py
@@ -1,20 +1,13 @@
-# lambda = 3 / const
-def from_lambda_to_const(lambda_) :
-  return 3 / lambda_
-
-
-def interpolate(x: list, y: list, threshold_x: float, threshold_y: float) ->list :
+def interpolate(x: list, y: list, threshold_x: float, threshold_slope: float) ->list :
   res = []
   for i in range(len(x)) :
     if i == 0:
       continue
     dx = x[i] - x[i - 1]
     dy = y[i] - y[i - 1]
-    # print(f'i = {i}, dy / dx = {abs(dy / dx)}, {abs(dx)}, {threshold_x}, {threshold_y}')
-    # print(f'dx = {dx}, lambda[{i}] = {x[i]}, lambda_[{i-1}] = {x[i - 1]}');
 
-    if abs(dy / dx) > threshold_y and abs(dy) > 0.03 and abs(dx) > threshold_x:
-      # print(f'############### i = {i}, dy / dx = {abs(dy / dx)}')
-      res.append((from_lambda_to_const(x[i]) + from_lambda_to_const(x[i-1]))/2) # 插点
+    if abs(dy / dx) > threshold_slope and abs(dy) > 0.03 and abs(dx) > threshold_x:
+      res.append((x[i] + x[i-1])/2) # 插点
+
   print(res)
   return res
